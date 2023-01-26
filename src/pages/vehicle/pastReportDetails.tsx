6aaa35418db3820292431d7067d386bd0476a3e9
kayd-kun <yesheykamal.89@gmail.com>
2023-01-26 10:38:16 +0600

    Demo

--- src/pages/vehicle/pastReportDetails.tsx
+++ src/pages/vehicle/pastReportDetails.tsx
@@ -7,12 +7,12 @@ import { BiSearchAlt } from "react-icons/bi";
 import { GoLocation } from "react-icons/go";
 import { AiOutlineCar } from "react-icons/ai";
 
-import { api } from "../utils/api";
+// import { api } from "../utils/api";
 import Image from "next/image";
 // import VehicleDetail from "../components/vehicleHome/vehicleDetail"; // Deleted
 
 export default function VehicleHome() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+//   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -22,9 +22,7 @@ export default function VehicleHome() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex justify-center flex-col">
-        <Link href="/vehicle/pastReportDetails">
-            Past Reports
-        </Link>
+        PAST REPORTSSSSSSSSSSSSSSS
         {/* Search and Check Status Button */}
         <div>
           <fieldset className="w-full space-y-1 dark:text-gray-100 py-4 flex justify-center">
@@ -117,28 +115,3 @@ export default function VehicleHome() {
     </>
   );
 }
-
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
---
+++ src/pages/vehicle/status.tsx
@@ -0,0 +1,18 @@
+import { BiArrowBack } from 'react-icons/bi'
+
+export default function VehicleStatus() {
+    return (
+        <>
+
+            <main>
+                {/* Title & Back arrow */}
+                <div className='flex flex-row justify-center gap-4 py-6'>
+                    <BiArrowBack className="w-7 h-7 text-gray-400 relative" />
+                    <span>
+                        Current Session
+                    </span>
+                </div>
+            </main>
+        </>
+    )
+}
